d03ef2966b80e36f0c29fec7fa3c808cb25f913d
Adi Narayana Vemuru <vemuruadi@gmail.com>
2020-11-11 20:23:54 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,18 @@
+<p align="center">
+  <img src="https://pbs.twimg.com/profile_images/1044156028915343361/uiUvIvLm_400x400.jpg" alt="Adi Vemuru">
+</p>
+
+```bash
+npx vemuruadi
+```
+```
+Adi Vemuru / vemuruadi / vemuruadi.com
+
+         Work: Fresh Qube, Founder - Building Customer-Led and Data-Driven Products with Cloud Native Technologies
+       Github: vemuruadi
+      Twitter: @vemuruadi
+     LinkedIn: linkedin.com/in/vemuruadi
+
+        Email: adi@vemuruadi.com
+         Card: npx vemuruadi
+```
---
+++ index.js
@@ -0,0 +1,19 @@
+
+const { white, cyan, bold, blue } = require('colorette')
+
+const info = {
+    Work: white(`${blue('Fresh Qube')}, Founder - Building Customer-Led and Data-Driven Products with Cloud Native Technologies`),
+    Github: cyan('vemuruadi'),
+    Twitter: cyan('vemuruadi'),
+    LinkedIn: cyan('linkedin.com/in/vemuruadi') + '\n' ,
+    Email: cyan('adi@vemuruadi.com'),
+    Card: white('npx vemuruadi'),
+}
+
+const head = `${blue(bold('  Adi Vemuru') + ' /')} ${blue('vemuruadi')} / ${white('vemuruadi.com')}`
+
+const body = Object.entries(info)
+  .map(([key, value]) => [white(bold(`${key}:`.padStart(16, ' '))), value].join(' '))
+  .join('\n')
+
+console.log('\n' + [head, body].join('\n\n') + '\n')
